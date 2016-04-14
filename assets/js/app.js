5196545d26f2add82749e8720753498c413e4993
kujtimiihoxha <kujtimii.h@gmail.com>
2016-04-14 15:16:38 +0200

    Edit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-angular.module("app",[ ]).config(function($interpolateProvider) {
+angular.module("kujtimBlog",[ ]).config(function($interpolateProvider) {
     $interpolateProvider.startSymbol('<%');
     $interpolateProvider.endSymbol('%>');
 });
