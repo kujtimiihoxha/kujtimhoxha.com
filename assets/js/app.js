b4b422286e563727f14d01fc34f448ae918da188
kujtimiihoxha <kujtimii.h@gmail.com>
2016-04-14 15:14:41 +0200

    Edit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,4 @@
 angular.module("app",[ ]).config(function($interpolateProvider) {
     $interpolateProvider.startSymbol('<%');
     $interpolateProvider.endSymbol('%>');
-}).run(function () {
-    console.log("Running");
-}).controller("test",function ($scope) {
-    $scope.test = "Hello";
-    console.log("Running");
 });
